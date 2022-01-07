7246fc2984dbbd659d4ad13dfd8c10a26dc9923d
mendespedro <windowsxpedro@gmail.com>
2022-01-07 18:19:34 -0300

    removing moduleraid

--- loadScripts.js
+++ loadScripts.js
@@ -1,5 +1,4 @@
 injectFunctionInstantly(webScoketInterception);
-injectFunctionInstantly(loadModuleRaid);
 loadScripts();
 
 async function loadScripts() {
@@ -155,66 +154,3 @@ function webScoketInterception()
     }
 
 }
-
-function loadModuleRaid() {
-    const moduleRaid = function () {
-        moduleRaid.mID  = Math.random().toString(36).substring(7);
-        moduleRaid.mObj = {};
-
-        fillModuleArray = function() {
-            webpackChunkwhatsapp_web_client.push([ // Patch: webpackChunkbuild -> webpackChunkwhatsapp_web_client
-                [moduleRaid.mID], {}, function(e) {
-                    Object.keys(e.m).forEach(function(mod) {
-                        moduleRaid.mObj[mod] = e(mod);
-                    })
-                }
-            ]);
-        }
-
-        fillModuleArray();
-
-        get = function get (id) {
-            return moduleRaid.mObj[id]
-        }
-
-        findModule = function findModule (query) {
-            results = [];
-            modules = Object.keys(moduleRaid.mObj);
-
-            modules.forEach(function(mKey) {
-                mod = moduleRaid.mObj[mKey];
-
-                if (typeof mod !== 'undefined') {
-                    if (typeof query === 'string') {
-                        if (typeof mod.default === 'object') {
-                            for (key in mod.default) {
-                                if (key == query) results.push(mod);
-                            }
-                        }
-
-                        for (key in mod) {
-                            if (key == query) results.push(mod);
-                        }
-                    } else if (typeof query === 'function') {
-                        if (query(mod)) {
-                            results.push(mod);
-                        }
-                    } else {
-                        throw new TypeError('findModule can only find via string and function, ' + (typeof query) + ' was passed');
-                    }
-
-                }
-            })
-
-            return results;
-        }
-
-        return {
-            modules: moduleRaid.mObj,
-            constructors: moduleRaid.cArr,
-            findModule: findModule,
-            get: get
-        }
-    }
-}
-
